f5b60737d29a9d288511fc0bb2e5e42227303c68
amanda-santos <amandasf0200@gmail.com>
2022-11-01 19:44:57 -0300

    (mobile)chore: add styles and loading component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components";
+import { SignIn } from "./src/screens";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
