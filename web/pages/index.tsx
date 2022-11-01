2aab4809d814588df33eab93f3f350fa0bcb41ed
amanda-santos <amandasf0200@gmail.com>
2022-11-01 19:10:45 -0300

    wip: 🚧 get number of pools on web app

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+type HomeProps = {
+  count: number;
+};
+
+export default function Home({ count }: HomeProps) {
+  return <h1>Number of pools: {count}</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count");
+  const data = await response.json();
+
+  console.log(data);
+
+  return {
+    props: {
+      count: data.count,
+    },
+  };
+};
