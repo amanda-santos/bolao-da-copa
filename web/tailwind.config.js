defede3d71e19689cfa33dba96ca2bfab504456d
amanda-santos <amandasf0200@gmail.com>
2022-11-03 20:33:26 -0300

    (web) feat: ✨ landing page UI

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+
+      backgroundImage: {
+        app: "url(/app-bg.png)",
+      },
+
+      colors: {
+        ignite: {
+          500: "#129e57",
+        },
+
+        yellow: {
+          700: "#e5cd3d",
+          500: "#f7dd43",
+        },
+
+        gray: {
+          900: "#121214",
+          800: "#202024",
+          600: "#323238",
+          300: "#8d8d99",
+          100: "#e1e1e6",
+        },
+      },
+    },
   },
   plugins: [],
 };
